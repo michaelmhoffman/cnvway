e3dbf26278771eb397774d5607621424e809e71d
Sera Riddhi <t114244uhn@mordor113.midearth.uhnresearch.ca>
2022-06-08 13:58:40 -0400

    add cnvway output

---
+++ segway_output/traindir/log/run.sh
@@ -0,0 +1,2 @@
+## segway 3.0.4 run 6e26b9a6e75311eca293525400b68424 at 2022-06-08 13:50:06.272007
+/mnt/work1/users/home2/t114244uhn/miniconda3/envs/cnvway/bin/gmtkTriangulate -cppCommandOptions '-DCARD_SEG=3 -DCARD_SUBSEG=1 -DCARD_FRAMEINDEX=67 -DSEGTRANSITION_WEIGHT_SCALE=1.0' -outputTriangulatedFile segway_output/traindir/triangulation/segway.str.3.1.trifile -strFile segway.str -verbosity 0
---
+++ segway_output/traindir/log/segway.sh
@@ -0,0 +1,4 @@
+## segway 3.0.4 run 6e26b9a6e75311eca293525400b68424 at 2022-06-08 13:50:06.272007
+
+cd "/mnt/work1/users/home2/t114244uhn/cnvway"
+"cnvway.py"
